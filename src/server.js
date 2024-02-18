934ce2ec2f15921c30383265f572a942ba036d37
Emerson MX <emersonmx@gmail.com>
2024-02-18 14:13:34 -0300

    Add babel and compression

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
+import compression from 'compression'
 import { router } from './routes/index.js'
 import config from './config.js'
 
 const app = express()
 
 app.use(helmet())
+app.use(compression())
 app.use(express.json())
 app.use(cors())
 
