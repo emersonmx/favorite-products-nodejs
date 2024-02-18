17ab8d2b02e73e8e714d6453ed9413446779b03b
Emerson MX <emersonmx@gmail.com>
2024-02-18 16:35:21 -0300

    Test customer happy path

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -2,8 +2,43 @@ const express = require('express')
 
 const router = express.Router()
 
+const customers = new Map()
+
 router.post('/', async (req, res) => {
-  res.status(201).location('http://localhost:3000/customers/1').send()
+  const id = crypto.randomUUID()
+  const { name, email } = req.body
+  customers.set(id, { id, name, email })
+  res.status(201).location(`http://localhost:3000/customers/${id}`).end()
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  if (!customers.has(id)) {
+    return res.status(404).end()
+  }
+
+  res.status(200).send(customers.get(id))
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  if (!customers.has(id)) {
+    return res.status(404).end()
+  }
+
+  const { name, email } = req.body
+  customers.set(id, { id, name, email })
+  res.status(200).end()
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  if (!customers.has(id)) {
+    return res.status(404).end()
+  }
+
+  customers.delete(id)
+  res.status(200).end()
 })
 
 module.exports = router
--- src/routes/customers.test.js
+++ src/routes/customers.test.js
@@ -1,13 +1,47 @@
 const axios = require('axios')
+const { expect } = require('@jest/globals')
 
 axios.defaults.baseURL = 'http://127.0.0.1:3000'
+axios.defaults.validateStatus = () => {
+  return true
+}
 
-test('create a new customer with name and email', async () => {
-  const res = await axios.post('/customers', {
-    name: 'John',
-    email: 'john@example.com'
+describe('happy path', () => {
+  let resCreate
+
+  beforeAll(async () => {
+    resCreate = await axios.post('/customers', {
+      name: 'John',
+      email: 'john@example.com'
+    })
   })
 
-  expect(res.status).toBe(201)
-  expect(res.headers.location).toMatch(/\/customers\/(.+)$/)
+  test('create with name and email', async () => {
+    expect(resCreate.status).toBe(201)
+    expect(resCreate.headers.location).toMatch(/\/customers\/(.+)$/)
+  })
+
+  test('show by id', async () => {
+    const res = await axios.get(resCreate.headers.location)
+
+    expect(res.status).toBe(200)
+    expect(res.data).toMatchObject({ name: 'John', email: 'john@example.com' })
+  })
+
+  test('update name and email', async () => {
+    const res = await axios.put(resCreate.headers.location, {
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
+    expect(res.status).toBe(200)
+  })
+
+  test('delete by id', async () => {
+    const resDelete = await axios.delete(resCreate.headers.location)
+    const resShow = await axios.get(resCreate.headers.location)
+
+    expect(resDelete.status).toBe(200)
+    expect(resShow.status).toBe(404)
+  })
 })
