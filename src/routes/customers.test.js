36fce9e38babc9260e71528207e3a87b35e0af39
Emerson MX <emersonmx@gmail.com>
2024-02-18 15:16:45 -0300

    Update customers test

--- src/routes/customers.test.js
+++ src/routes/customers.test.js
@@ -1,11 +1,13 @@
 const axios = require('axios')
 
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
+
 test('create a new customer with name and email', async () => {
-  const res = await axios.post('http://localhost:3000/customers', {
+  const res = await axios.post('/customers', {
     name: 'John',
     email: 'john@example.com'
   })
 
   expect(res.status).toBe(201)
-  expect(res.headers.location).toBe('http://localhost:3000/customers/1')
+  expect(res.headers.location).toMatch(/\/customers\/(.+)$/)
 })
