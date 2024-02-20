f82b2ac5589384e53773cca4310467876feb7521
Emerson MX <emersonmx@gmail.com>
2024-02-20 18:54:44 -0300

    Add empty handlers to product-list

--- src/routes/product-list.js
+++ src/routes/product-list.js
@@ -1,3 +1,7 @@
+async function list(request, reply) {
+  return {}
+}
+
 async function create(request, reply) {
   const id = crypto.randomUUID()
 
@@ -5,6 +9,14 @@ async function create(request, reply) {
     .code(201)
     .header('location', encodeURI(`/product-list/${id}`))
     .send({ id })
+}
+
+async function show(request, reply) {
+  return {}
+}
+
+async function destroy(request, reply) {
+  return {}
 }
 
 module.exports = async (fastify, options) => {
@@ -29,6 +41,8 @@ module.exports = async (fastify, options) => {
 
   const baseBodySchema = S.object()
     .prop('id', S.string().format(S.FORMATS.UUID).required())
+  const paramsSchema = S.object()
+    .prop('id', S.string().format(S.FORMATS.UUID).required())
   const responseSchema = S.object()
     .prop('id', S.string().format(S.FORMATS.UUID))
     .prop('title', S.string())
@@ -38,6 +52,15 @@ module.exports = async (fastify, options) => {
     .prop('reviewScore', S.number())
   const notFoundSchema = S.null().raw({ description: "Product not found" })
   const productExistsSchema = S.null().raw({ description: 'Product already exists' })
+
+  fastify.get('/', {
+    schema: {
+      tags: ['product-list'],
+      response: {
+        200: responseSchema,
+      }
+    }
+  }, list)
 
   fastify.post('/', {
     schema: {
@@ -49,4 +72,26 @@ module.exports = async (fastify, options) => {
       }
     }
   }, create)
+
+  fastify.get('/:id', {
+    schema: {
+      tags: ['product-list'],
+      params: paramsSchema,
+      response: {
+        200: responseSchema,
+        404: notFoundSchema
+      }
+    }
+  }, list)
+
+  fastify.delete('/:id', {
+    schema: {
+      tags: ['product-list'],
+      params: paramsSchema,
+      response: {
+        200: S.null(),
+        404: notFoundSchema
+      }
+    }
+  }, list)
 }
