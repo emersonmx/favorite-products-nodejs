9397cfea5c179b2ce456956b09cba3c7513b10d8
Emerson MX <emersonmx@gmail.com>
2024-02-21 15:44:01 -0300

    Export usecases

--- src/usecases/index.js
+++ src/usecases/index.js
@@ -1,8 +1,13 @@
 module.exports = {
   Errors: require('./errors'),
   CreateCustomer: require('./create-customer'),
-  FindCustomerById: require('./find-customer-by-id'),
+  DeleteCustomer: require('./delete-customer'),
+  FavoriteProduct: require('./favorite-product'),
   FindCustomerByEmail: require('./find-customer-by-email'),
+  FindCustomerById: require('./find-customer-by-id'),
+  FindProductById: require('./find-product-by-id'),
+  ListCustomerFavoriteProducts: require('./list-customer-favorite-products'),
+  UnfavoriteProduct: require('./unfavorite-product'),
   UpdateCustomer: require('./update-customer'),
-  DeleteCustomer: require('./delete-customer')
 }
+
