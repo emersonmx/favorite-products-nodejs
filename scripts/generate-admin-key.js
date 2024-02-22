8456440739dde9eb2184be98e19a6e14c4bf93a5
Emerson MX <emersonmx@gmail.com>
2024-02-21 21:02:38 -0300

    Update generate-admin-key script

--- scripts/generate-admin-key.js
+++ scripts/generate-admin-key.js
@@ -3,9 +3,9 @@
   const { createSigner } = require('fast-jwt')
   const config = require(path.resolve(__dirname, '../src/config'))
 
-  const adminSignSync = createSigner({ key: config.adminJwtSecret })
+  const signSync = createSigner({ key: config.adminJwtSecret })
 
-  const key = adminSignSync({ sub: 'admin@example.com', name: 'Admin' })
+  const key = signSync({ sub: 'admin@example.com', name: 'Admin' })
 
   console.log(key)
 })()
